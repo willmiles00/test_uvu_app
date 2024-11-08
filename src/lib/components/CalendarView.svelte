224b4c219f83a4244508ad5a624729147f75f259
Will <113927655+willmiles00@users.noreply.github.com>
2024-11-07 20:22:28 -0700

    deobscurification init

---
+++ src/lib/components/CalendarView.svelte
@@ -0,0 +1,22 @@
+
+<script lang="ts">
+    //I was able to remove an error about declaration by running 'npm install --save-dev @types/event-calendar__core'
+    import Calendar from '@event-calendar/core';
+
+    //I was able to remove an error about declaration by running 'npm install --save-dev @types/event-calendar__time-grid'
+    import TimeGrid from '@event-calendar/time-grid';
+
+    let plugins = [TimeGrid];
+    let options = {
+        view: 'timeGridWeek',
+        events: [
+            {
+                title: 'Event 1',
+                start: '2024-11-07T01:00:00',
+                end: '2024-11-07T02:00:00'
+            },
+        ]
+    };
+</script>
+
+<Calendar {plugins} {options} />
