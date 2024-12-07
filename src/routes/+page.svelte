530a6483ad140fddf91e1f7271436accb7d5c243
Will <113927655+willmiles00@users.noreply.github.com>
2024-12-07 09:45:58 -0700

    some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 	import { ProgressRadial } from '@skeletonlabs/skeleton';
 
 	// modal setters
-	let isUploadModalActive = false;
+	let isUploadModalActive = true;
 	function handleUploadModal() {
 		isUploadModalActive = !isUploadModalActive;
 	}
@@ -26,13 +26,18 @@
 	let csvData: any[] = [];
 	let file: any;
 	let loadingPromise: Promise<number>;
+		let fileName = "No file chosen";
 
-	//   this submits the upload form and grabs the file
-	function handleUpload(event: { target: any }) {
-		file = event.target[0].files[0];
-		parseCSV();
-		// handleUploadModal();
-	}
+	// this function handles the file change event, so that parsing can be done
+  function handleFileChange(event: any) {
+    file = event.target.files[0];
+    if (file) {
+      fileName = file.name;
+    } else {
+      fileName = "No file chosen";
+    }
+  }
+
 
 	// this takes the uploaded file and parses it into a usable format
 	function parseCSV() {
@@ -133,11 +138,19 @@
 		<div
 			class="bg-black bg-opacity-50 w-full h-fit absolute top-0 left-0 min-h-full  flex justify-center"
 		>
-			<div class="flex flex-wrap bg-gray-400 m-10 p-10 rounded-xl w-fit md:min-w-[620px] h-fit max-h-[600px] overflow-auto shadow-xl custom-scrollbar">
+		<div class="relative">
+			<div class="flex flex-wrap bg-white m-10 p-10 rounded-xl w-fit md:min-w-[620px] max-w-[620px] h-fit max-h-[600px] overflow-auto shadow-xl custom-scrollbar ">
+				<button class="absolute top-8 right-[4.5%]  font-bold h-[30px] w-[30px] bg-red-500 p-2 rounded-full flex flex-col justify-center items-center shadow-2xl"><img src="/xmark-solid.svg" alt="close div"></button>
 				{#if csvData.length < 1}
-				<p class="w-full text-xl text-uvu-green font-bold text-center">Upload a Schedule</p>
-				<form class="w-full" on:submit|preventDefault={handleUpload}>
-					<input class="input text-black max-w-[620px]" type="file" accept=".csv" name="fileUpload" />
+				<p class="w-full text-xl text-uvu-green font-bold text-center m-5">Upload a Schedule</p>
+				<form class="w-full" on:submit|preventDefault={parseCSV}>
+					<button type="button" class=" w-full flex flex-wrap justify-center items-center">
+					<label for="file-upload" class="custom-file-upload flex flex-col justify-center items-center">
+						<img src="/arrow-up-solid.svg" alt="upload arrow" class="h-[60px] w-[60px] p-2 bg-uvu-green rounded-full cursor-pointer">
+					<p>{fileName}</p>
+					</label>
+				</button>
+					<input class="my-3 input text-black max-w-[620px] rounded-none bg-white border-2 !border-black" type="file" accept=".csv" name="fileUpload" id="file-upload" on:change={handleFileChange}/>
 					<button class="btn bg-gradient-to-br variant-gradient-primary-secondary" type="submit"
 						>Upload</button
 					>
@@ -170,6 +183,7 @@
 			
 			</div>
 		</div>
+		</div>
 	{/if}
 </main>
 
@@ -178,4 +192,10 @@
 	  scrollbar-color: red orange !important;
 	  scrollbar-width: thick !important;
 	} */
+
+	input[type="file"] {
+  display: none;
+}
+
+
 </style>
