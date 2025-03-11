8a419095eab55f0c62fb0e28eb430a7eed905815
Will <113927655+willmiles00@users.noreply.github.com>
2025-03-11 02:05:35 -0600

    styled upload modal

--- src/lib/components/modals/UploadModal.svelte
+++ src/lib/components/modals/UploadModal.svelte
@@ -42,10 +42,10 @@
 	>
 	  <div class="relative">
 		<div
-		  class="flex flex-wrap bg-white m-10 rounded-[8px] w-fit md:min-w-[620px] max-w-[620px] h-fit max-h-[600px] overflow-auto shadow-xl custom-scrollbar upload-modal min-h-[226px] "
+		  class="flex flex-wrap bg-white m-10 rounded-[8px] w-fit md:min-w-[486px] max-w-[486px] h-fit max-h-[600px] overflow-auto shadow-xl custom-scrollbar upload-modal min-h-[226px] "
 		>
 		  <p
-			class="w-full text-white uppercase bg-[#00843D] text-[31px] flex items-center px-[24px] py-[12px] font-primary-semibold"
+			class="w-full text-white uppercase bg-[#00843D] text-[24px] flex items-center px-[24px] py-[12px] font-primary-semibold h-[55px]"
 		  >
 			Import .CSV
 		  </p>
@@ -55,22 +55,25 @@
 				for="file-upload"
 				class="custom-file-upload flex flex-wrap justify-start items-center mt-2 w-[438px] rounded-[8px]"
 			  >
+
 				<p
-				  class="flex flex-wrap justify-center items-center btn bg-uvu-green text-white p-2 uppercase font-[16px] font-bold rounded-[8px]"
+				  class="flex flex-wrap justify-center items-center btn bg-[#275D38] text-white p-2 uppercase text-[16px] font-semibold rounded-tl-[8px] rounded-bl-[8px] rounded-tr-none rounded-br-none  font-roboto relative"
 				>
 				  Choose File
 				</p>
-				{#if fileName === 'No file chosen'}
-				  <p
-					class="text-[#DDDDDD] border-[#DDDDDD] border-2 rounded-[8px] p-[6px] border-l-0"
-				  >
-					{fileName}
-				  </p>
-				{:else}
-				  <p class="border-[#DDDDDD] border-2 rounded-[8px] h-full p-[6px] border-l-0">
-					{fileName}
-				  </p>
-				{/if}
+				<div class="absolute top-[119px] left-[64px] mt-2 w-[438px] border-[#DDDDDD] border rounded-[8px] h-[43px]">
+				  {#if fileName === 'No file chosen'}
+					<p
+					  class="text-[#DDDDDD] p-2 pl-[140px]"
+					>
+					  {fileName}
+					</p>
+				  {:else}
+					<p class="p-2 pl-[140px]">
+					  {fileName}
+					</p>
+				  {/if}
+				</div>
 			  </label>
 			</div>
   
@@ -82,15 +85,15 @@
 			  id="file-upload"
 			  on:change={handleFileChange}
 			/>
-			<div class="flex m-2 justify-end">
+			<div class="flex m-2 justify-end mr-6 mt-[24px]">
 			  <button
-				class="bg-[#DDDDDD] p-2 rounded-md flex flex-col justify-center items-center text-uvu-green mr-2 uppercase text-sm"
+				class="bg-[#DDDDDD] p-2 rounded-md flex flex-col justify-center items-center text-uvu-green mr-2 uppercase text-sm font-rajdhani h-[40px] font-semibold w-[92px]"
 				type="button"
 				on:click={close}>Cancel</button
 			  >
 			  <button
-				class="bg-[#DDDDDD] p-2 rounded-md flex flex-col justify-center items-center text-uvu-green uppercase text-sm"
-				type="submit">Upload</button
+				class="bg-uvu-green p-2 rounded-md flex flex-col justify-center items-center text-white uppercase text-sm font-rajdhani h-[40px] w-[122px] font-semibold"
+				type="submit">Upload .CSV</button
 			  >
 			</div>
 		  </form>
@@ -107,5 +110,9 @@
 	.upload-modal {
 	  overflow: scroll !important;
 	  overflow-y: scroll;
+	}
+
+	.font-roboto {
+	  font-family: 'Roboto', sans-serif;
 	}
   </style>
